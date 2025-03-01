efd35658b93b7e226f3c3806c5c998df8797b103
ChrishelleC <culmerchrishelle12@gmail.com>
2025-02-28 22:35:25 -0500

    the calculations

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,45 @@ class CalculatorMain extends StatefulWidget {
 }
 
 class _CalculatorMainState extends State<CalculatorMain> {
-  String input = ""; //variable for user input
-  String output = ""; //variable for result
+  String input = ""; //stores user input
+  String output = ""; //stores result for result
   double num1 = 0;
   double num2 = 0;
   String operator = "";
+
+  void buttonPressed(String value) {
+    setState(() {
+      //clear values if C is pressed
+      if (value == "C") {
+        input = "";
+        output = "";
+        num1 = 0;
+        num2 = 0;
+        operator = "";
+      }
+      //calculate numbers after "=" is pressed
+      else if (value == "=" && operator.isNotEmpty) {
+      num2 = double.parse(input);
+      if (operator == "+") {
+        output = (num1 + num2).toString();
+        }else if (operator == "-") {
+          output = (num1 - num2).toString();
+        }else if (operator == "*") {
+          output = (num1 * num2).toString();
+        }else if (operator == "/") {
+          output = (num1 / num2).toString();
+        }else {
+          output = "Error";
+        }
+        input = "";
+        operator = "";
+      } else if (["+", "-", "*", "/"].contains(value)) {
+        if (input.isNotEmpty) {
+          num1 = double.parse(input);
+          operator = value;
+          input = "";
+        }
+      }
+    });
+  }
 }
